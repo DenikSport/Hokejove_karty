3a6f7ba2bcaa9cb9967a67346b0573bdd7332737
Deník sport <111072365+DenikSport@users.noreply.github.com>
2023-12-15 13:20:25 +0100

    Update Hokejove_karty.py

--- Hokejove_karty.py
+++ Hokejove_karty.py
@@ -11,7 +11,7 @@ st.title("Vítejte v této aplikaci! ⚽")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/DenikSport/Hokejove_karty/main/Database.csv")
+    df = pd.read_csv("https://raw.githubusercontent.com/DenikSport/Hokejove_karty/main/Database.csv",encoding='utf-32')
     return df
 
 df = load_data() 
