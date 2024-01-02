67fd50b957bd456aa83c20d9eb242367803e84cd
Deník sport <111072365+DenikSport@users.noreply.github.com>
2024-01-02 15:21:46 +0100

    Update Hokejove_karty.py

--- Hokejove_karty.py
+++ Hokejove_karty.py
@@ -362,10 +362,10 @@ image.save(buffered, format="PNG")
 img_data = buffered.getvalue()
 file_name = f"Hokejová karta - {selected_player} iSport.png"
 
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3,col4, col5 = st.columns([1,1,2,1,1])
 
 # Vycentrování tlačítka pro stažení ve středním sloupci
-with col2:
+with col3:
     st.download_button(
         label="Stáhnout obrázek",
         data=img_data,
