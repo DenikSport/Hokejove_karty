7b7fd191b16295e559ca31eac08a249f850121bc
Deník sport <111072365+DenikSport@users.noreply.github.com>
2024-02-21 17:58:15 +0100

    Update Hokejove_karty.py

--- Hokejove_karty.py
+++ Hokejove_karty.py
@@ -377,3 +377,20 @@ image.paste(logo_image2, (x, y), logo_image2)
 
 
 st.image(image)
+
+
+buffered = io.BytesIO()
+image.save(buffered, format="PNG")
+img_data = buffered.getvalue()
+file_name = f"Hokejová karta - {selected_player}.png"
+
+col1, col2, col3 = st.columns([1.5,1,1.5])
+
+# Vycentrování tlačítka pro stažení ve středním sloupci
+with col2:
+    st.download_button(
+        label="Stáhnout kartu hráče",
+        data=img_data,
+        file_name=file_name,
+        mime="image/png"
+    )
