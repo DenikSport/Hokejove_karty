34b28f7bbb6ce3246c1b90c190e5fcf71ca2c343
Deník sport <111072365+DenikSport@users.noreply.github.com>
2024-02-26 20:45:55 +0100

    Create Kontrola.py

---
+++ Kontrola.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import os
+
+
+st.markdown("""
+    <style>
+    .stApp {
+        background-color: #2a2a2c;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Načtení nahráného CSV souboru
+data = pd.read_csv('https://raw.githubusercontent.com/DenikSport/Hokejove_karty/main/List.csv', sep = ";")
+
+# Vytvoření selectboxu pro kluby
+kluby = ['Všechny kluby'] + sorted(data['Tym'].unique().tolist())
+vybrany_klub = st.selectbox('', kluby)
+
+# Filtrování dat podle vybraného klubu
+if vybrany_klub != 'Všechny kluby':
+    data = data[data['Tym'] == vybrany_klub]
+
+# Vytvoření seznamu hráčů na základě filtrovaných dat
+player_list = pd.unique(data[['Jmeno']].values.ravel())
+
+# Vytvoření selectboxu pro hráče
+selected_player = st.selectbox("", player_list)
+
+# Získání dat pro vybraného hráče
+player_data = data[data['Jmeno'] == selected_player].iloc[0]
+
+# Zobrazení informací o hráči a jeho obrázku
+
+# Načtení a zobrazení obrázku hráče
+image_path = player_data['image_path']
+if os.path.exists(image_path):
+    image = Image.open(image_path)
+    st.image(image, caption=player_data['Jmeno'])
