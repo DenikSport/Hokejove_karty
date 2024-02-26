44fa10ceb958070effd46821f4664f3c115d1206
Deník sport <111072365+DenikSport@users.noreply.github.com>
2024-02-26 21:06:41 +0100

    Update Kontrola.py

--- Kontrola.py
+++ Kontrola.py
@@ -12,13 +12,9 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-
-
 # Načtení nahráného CSV souboru
 data = pd.read_csv('https://raw.githubusercontent.com/DenikSport/Hokejove_karty/main/List.csv', sep=";", dtype={'Jmeno': 'str', 'Tym': 'str', 'image_path': 'str'})
 
-
-st.write(data)
 # Vytvoření selectboxu pro kluby
 kluby = ['Všechny kluby'] + sorted(data['Tym'].unique().tolist())
 vybrany_klub = st.selectbox('Vyberte hráče', kluby, label_visibility="collapsed")
@@ -35,7 +31,6 @@ selected_player = st.selectbox("Vyberte Tým", player_list, label_visibility="co
 
 # Získání dat pro vybraného hráče
 player_data = data[data['Jmeno'] == selected_player].iloc[0]
-st.write(player_data)
 # Zobrazení informací o hráči a jeho obrázku
 
 # Načtení a zobrazení obrázku hráče
@@ -44,4 +39,4 @@ image_path = player_data['image_path']
 st.write(image_path)
 if os.path.exists(image_path):
     image = Image.open(image_path)
-    st.image(image, caption=player_data['Jmeno'])
+    st.image(image)
